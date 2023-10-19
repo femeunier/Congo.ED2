c475c8e3f7d007e0338b928c4ef5eb8ec36012ff
femeunier <felicien.meunier@gmail.com>
2023-10-19 13:08:29 +0200

    Add find.coord.raster

--- R/find.coord.raster.R
+++ R/find.coord.raster.R
@@ -4,6 +4,7 @@ find.coord.raster <- function(craster.df,target,Ngridcells = 1){
   clat <- target[2]
 
   model.lat.lons <-  craster.df %>%
+    group_by(model) %>%
     mutate(dist = sqrt((clat - lat)**2 + (clon - lon)**2)) %>%
     dplyr::filter(dist %in% sort(unique(dist))[1:Ngridcells]) %>%
     pull(model.lat.lon)
