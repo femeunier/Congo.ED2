fcc686d71558dd991ab91396b9089b3eb413ea37
femeunier <felicien.meunier@gmail.com>
2023-11-23 09:17:30 +0100

    Enlarge America

--- R/coord2continent.R
+++ R/coord2continent.R
@@ -1,7 +1,7 @@
 coord2continent <- function(lon,lat){
 
   continent = rep(NA_character_,length(lon))
-  continent[lon <= -20 & lon >= -85] <- "America"
+  continent[lon <= -20 & lon >= -120] <- "America"
   continent[lon > -20 & lon <= 60 ] <- "Africa"
   continent[lon > 60] <- "Australasia"
   # continent[lon > 90 & lon < 175 & lat > - 10] <- "Asia"
