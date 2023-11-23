beb0e9066352a99c9742d95eaa03544f935b9c80
femeunier <felicien.meunier@gmail.com>
2023-11-23 09:08:31 +0100

    Australasia

--- R/coord2continent.R
+++ R/coord2continent.R
@@ -3,11 +3,14 @@ coord2continent <- function(lon,lat){
   continent = rep(NA_character_,length(lon))
   continent[lon <= -20 & lon >= -85] <- "America"
   continent[lon > -20 & lon <= 55 ] <- "Africa"
-  continent[lon > 90 & lon < 175 & lat > - 10] <- "Asia"
-  continent[ lon > 100 & lon < 175 & lat <= -10] <- "Australia"
+  continent[lon > 55] <- "Australasia"
+  # continent[lon > 90 & lon < 175 & lat > - 10] <- "Asia"
+  # continent[ lon > 100 & lon < 175 & lat <= -10] <- "Australia"
+  # continent <- factor(continent,
+  #                     levels = c("America","Africa","Asia","Australia"))
 
   continent <- factor(continent,
-                      levels = c("America","Africa","Asia","Australia"))
+                      levels = c("America","Africa","Australasia"))
   return(continent)
 
 }
